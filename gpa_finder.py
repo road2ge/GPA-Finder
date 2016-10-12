61c76a3a5e9476e99d835aca4f4cad08937c01cb
kmiller <kalemiller@simplecyberspace.com>
2016-10-12 14:00:59 -0400

    actually updated
    now works eggs dee

--- gpa_finder.py
+++ gpa_finder.py
@@ -3,8 +3,8 @@ from __future__ import print_function
 cp_gpa = []
 h_gpa = []
 ap_gpa = []
-average = 50
-for whattonamethis in range(0, 50):
+average = 40
+for whattonamethis in range(0, 41):
     cp_gpa.append(whattonamethis * .1)
     h_gpa.append(whattonamethis * .1 + .5)
     ap_gpa.append(whattonamethis * .1 + 1)
@@ -15,33 +15,40 @@ cp_classes = int(input('And how many of those classes were weighted CP (standard
 h_classes = int(input('And how many of those classes were weighted Honors? '))
 # To confirm.  That way, the user can just cancel.
 print('So that means you were enrolled in', (number_of_classes - cp_classes) - h_classes, 'AP classes.')
-correct = input('Is that correct (y/n)? ')
+correct = raw_input('Is that correct (y/n)? ')
+correct.upper()
+correct.lower()
 
-if correct in 'yY':
-	print('I\'m going to have to ask you some questions now. Unfortunately, I would make this',
+if correct.lower() == 'y':
+    print('I\'m going to have to ask you some questions now. Unfortunately, I would make this',
        'sound a little smarter, but Python\'s way of getting input from you from the console is a little annoying. Bear with me though.')
 
-gpas = []
-for cp_class_grade in range(0, cp_classes):
-    num_grade = int(input('What was your grade for one of your CP classes? '))
-    gpa = cp_gpa[num_grade - 51]
-    gpas.append(gpa)
+    gpas = []
+    for cp_class_grade in range(0, cp_classes):
+        num_grade = int(input('What was your grade for one of your CP classes? '))
+        gpa = cp_gpa[num_grade - 59]
+        print(gpa)
+        gpas.append(gpa)
 
-for h_class_grade in range(0, h_classes):
-    num_grade = int(input('What was your grade for one of your Honors classes? '))
-    gpa = h_gpa[num_grade - 51]
-    gpas.append(gpa)
+    for h_class_grade in range(0, h_classes):
+        num_grade = int(input('What was your grade for one of your Honors classes? '))
+        gpa = h_gpa[num_grade - 59]
+        gpas.append(gpa)
+        print(gpa)
 
-for ap_class_grade in range(0, (number_of_classes - cp_classes) - h_classes):
-    num_grade = int(input('What was your grade for one of your AP classes? '))
-    gpa = ap_gpa[num_grade - 51]
-    gpas.append(gpa)
+    for ap_class_grade in range(0, (number_of_classes - cp_classes) - h_classes):
+        num_grade = int(input('What was your grade for one of your AP classes? '))
+        gpa = ap_gpa[num_grade - 59]
+        gpas.append(gpa)
+        print(gpa)
 
-	total_gpa = 0.0    
-	for gpa in range(0, len(gpas)):
-    	total_gpa += gpas[gpa]
-
-	avg_gpa = total_gpa / len(gpas)
-	print('You\'re total weighted South Carolina GPA is ', avg_gpa, '!', sep='')
+        total_gpa = 0.0    
+    def get_average_gpa(gpalist):
+        gpasum = 0
+        for gpa in gpalist:
+            gpasum += gpa
+        return gpasum / len(gpalist)
+    avg_gpa = get_average_gpa(gpas)
+    print('Your average weighted South Carolina GPA is ', avg_gpa, '!', sep='')
 else:
-	print('Well, you screwed up, not me.')
+    print('Well, you screwed up, not me.')
